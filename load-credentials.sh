51ff15473db94bb9369222cbabb5b4916f5de356
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-06-24 16:40:22 +0200

    refactor: more straightforward env setup

---
+++ load-credentials.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+set -e
+
+source "${DIR}/../.env"
+
+DVC_S3_CREDENTIALS_OUT_PATH="${DIR}/../.dvc/aws.credentials"
+OVHAI_CREDENTIALS_OUT_PATH="${DIR}/../env/ovhai.credentials.env"
+
+unlock_bitwarden() {
+    echo "# Unlocking bitwarden"
+    BW_SESSION="$(bw unlock --raw)"
+
+    echo "Done"
+    echo ""
+}
+
+load_dvc_s3_credentials() {
+    echo "# Loading DVC credentials from bitwarden"
+
+    AWS_ACCESSKEYID=$(bw get item "${BITWARDEN_DVC_S3_CREDENTIALS_SECRET}" | jq --raw-output '.fields[] | select(.name | contains("accesskeyid")) | .value')
+    AWS_SECRETACCESSKEY=$(bw get item "${BITWARDEN_DVC_S3_CREDENTIALS_SECRET}" | jq --raw-output '.fields[] | select(.name | contains("secretaccesskey")) | .value')
+
+    if [ -z "${AWS_ACCESSKEYID}" ]; then echo "Could not load '.fields[name=accesskeyid].value' from bitwarden secret ${BITWARDEN_DVC_S3_CREDENTIALS_SECRET}"; exit 1; fi
+    if [ -z "${AWS_SECRETACCESSKEY}" ]; then echo "Could not load '.fields[name=secretaccesskey].value' from bitwarden secret ${BITWARDEN_DVC_S3_CREDENTIALS_SECRET}"; exit 1; fi
+
+    echo "[default]
+aws_access_key_id=${AWS_ACCESSKEYID}
+aws_secret_access_key=${AWS_SECRETACCESSKEY}
+    " > "${DVC_S3_CREDENTIALS_OUT_PATH}"
+
+    echo "Done"
+    echo ""
+}
+
+load_ovhai_credentials() {
+    echo "# Loading OVHAI credentials from bitwarden"
+
+    OVHAI_USERNAME=$(bw get username "${BITWARDEN_OVHAI_SECRET}")
+    OVHAI_PASSWORD=$(bw get password "${BITWARDEN_OVHAI_SECRET}")
+    OVHAI_PROJECTID=$(bw get item "${BITWARDEN_OVHAI_SECRET}" | jq --raw-output '.fields[] | select(.name | contains("projectid")) | .value')
+    OVHAI_REGION=$(bw get item "${BITWARDEN_OVHAI_SECRET}" | jq --raw-output '.fields[] | select(.name | contains("region")) | .value')
+    OVHAI_DOCKER_REGISTRY=$(bw get item "${BITWARDEN_OVHAI_SECRET}" | jq --raw-output '.fields[] | select(.name | contains("docker-registry")) | .value')
+
+    if [ -z "${OVHAI_USERNAME}" ]; then echo "Could not load '.username' from bitwarden secret ${BITWARDEN_OVHAI_SECRET}"; exit 1; fi
+    if [ -z "${OVHAI_PASSWORD}" ]; then echo "Could not load '.password' from bitwarden secret ${BITWARDEN_OVHAI_SECRET}"; exit 1; fi
+    if [ -z "${OVHAI_PROJECTID}" ]; then echo "Could not load '.fields[name=projectid].value' from bitwarden secret ${BITWARDEN_OVHAI_SECRET}"; exit 1; fi
+    if [ -z "${OVHAI_REGION}" ]; then echo "Could not load '.fields[name=region].value' from bitwarden secret ${BITWARDEN_OVHAI_SECRET}"; exit 1; fi
+    if [ -z "${OVHAI_DOCKER_REGISTRY}" ]; then echo "Could not load '.fields[name=docker-registry].value' from bitwarden secret ${BITWARDEN_OVHAI_SECRET}"; exit 1; fi
+
+    echo "
+OVHAI_USERNAME=${OVHAI_USERNAME}
+OVHAI_PASSWORD=${OVHAI_PASSWORD}
+OVHAI_PROJECTID=${OVHAI_PROJECTID}
+OVHAI_REGION=${OVHAI_REGION}
+OVHAI_DOCKER_REGISTRY=${OVHAI_DOCKER_REGISTRY}
+    " > "${OVHAI_CREDENTIALS_OUT_PATH}"
+
+    echo "Done"
+    echo ""
+}
+
+unlock_bitwarden
+load_dvc_s3_credentials
+load_ovhai_credentials
+
+echo "# Loading credentials done"
--- setup.sh
+++ setup.sh
@@ -3,54 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 set -e
 
-PROJECT_DIR="${DIR}/.."
-source "${DIR}/../.env"
-source "${DIR}/ovhai.credentials.env"
+if ! which bw >/dev/null; then echo "Please install bitwarden cli https://bitwarden.com/help/cli/"; exit 1; fi
+if ! which jq >/dev/null; then echo "Please install jq cli"; exit 1; fi
+if ! which docker >/dev/null; then echo "Please install docker cli"; exit 1; fi
 
-if [ -z "${OVHAI_PLATFORM}" ]; then echo "Missing env OVHAI_PLATFORM in .env"; exit 1; fi
-if [ -z "${IMAGE_NAME}" ]; then echo "Missing env IMAGE_NAME in .env"; exit 1; fi
-
-if [ -z "${OVHAI_USERNAME}" ]; then echo "Missing env OVHAI_USERNAME in env/ovhai.credentials.env"; exit 1; fi
-if [ -z "${OVHAI_PASSWORD}" ]; then echo "Missing env OVHAI_PASSWORD in env/ovhai.credentials.env"; exit 1; fi
-if [ -z "${OVHAI_PROJECTID}" ]; then echo "Missing env OVHAI_PROJECTID in env/ovhai.credentials.env"; exit 1; fi
-if [ -z "${OVHAI_REGION}" ]; then echo "Missing env OVHAI_REGION in env/ovhai.credentials.env"; exit 1; fi
-if [ -z "${OVHAI_DOCKER_REGISTRY}" ]; then echo "Missing env OVHAI_DOCKER_REGISTRY in env/ovhai.credentials.env"; exit 1; fi
-
-
-install_ovhai() {
-    echo "# Installing ovhai cli for platform ${OVHAI_PLATFORM} and region ${OVHAI_REGION}"
-    rm -rf ovhai ovhai*.zip
-    OVHAI_REGION_LOWERCASE=$(echo "${OVHAI_REGION}" | awk '{print tolower($0)}')
-    curl -O "https://cli.${OVHAI_REGION_LOWERCASE}.training.ai.cloud.ovh.net/ovhai-${OVHAI_PLATFORM}.zip"
-    echo "Unzipping ovhai"
-    unzip "ovhai-${OVHAI_PLATFORM}.zip"
-    rm -rf ovhai*.zip
-    echo "Ovhai bin available"
-    chmod +x ./ovhai
-    echo "Done"
-    echo ""
-}
-
-login_to_ovhai() {
-    echo "# Logging in to ovhai platform"
-    ./ovhai login --username "${OVHAI_USERNAME}" --password "${OVHAI_PASSWORD}"
-    ./ovhai me --output json > ovhai.me.json
-    echo "Done"
-    echo ""
-}
-
-login_to_ovh_docker_registry() {
-    echo "Logging in to docker registry"
-    REGISTRY_PREFIX="${OVHAI_DOCKER_REGISTRY}/${OVHAI_PROJECTID}"
-    docker login --username "${OVHAI_USERNAME}" --password "${OVHAI_PASSWORD}" "${REGISTRY_PREFIX}"
-    echo -n "${REGISTRY_PREFIX}" > docker-registry-prefix.value
-    echo "Done"
-    echo ""
-}
-
-install_ovhai
-login_to_ovhai
-login_to_ovh_docker_registry
-
-
-echo "All done. REGION=${OVHAI_REGION}; REGISTRY_PREFIX=${REGISTRY_PREFIX}"
+"${DIR}/load-credentials.sh"
+"${DIR}/setup-ovhai.sh"
