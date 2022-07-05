5d1070292108d91b526ec0006c130a44cbf51fa9
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-07-05 18:57:11 +0200

    refactor: directory for multiple jobs

--- train-on-ovhai.sh
+++ train-on-ovhai.sh
@@ -8,17 +8,15 @@ source "${DIR}/../.env"
 source "${DIR}/ovhai.credentials.env"
 if [ -z "${IMAGE_NAME}" ]; then echo "Missing env IMAGE_NAME in .env"; exit 1; fi
 if [ -z "${OVHAI_REGION}" ]; then echo "Missing env OVHAI_REGION in env/ovhai.credentials.env"; exit 1; fi
-if [ -z "${JOB_STATUS_JSON_FILE}" ]; then echo "Missing env JOB_STATUS_JSON_FILE in .env file"; exit 1; fi
-if [ -z "${JOB_STATUS_ID_FILE}" ]; then echo "Missing env JOB_STATUS_ID_FILE in .env file"; exit 1; fi
 if [ -z "${VOLUME_DATA_NAME}" ]; then echo "Missing env VOLUME_DATA_NAME in .env file"; exit 1; fi
 if [ -z "${VOLUME_DATA_DIR}" ]; then echo "Missing env VOLUME_DATA_DIR in .env file"; exit 1; fi
 if [ -z "${VOLUME_DATA_MOUNT}" ]; then echo "Missing env VOLUME_DATA_MOUNT in .env file"; exit 1; fi
 if [ -z "${VOLUME_SRC_NAME}" ]; then echo "Missing env VOLUME_SRC_NAME in .env file"; exit 1; fi
 if [ -z "${VOLUME_SRC_DIR}" ]; then echo "Missing env VOLUME_SRC_DIR in .env file"; exit 1; fi
 if [ -z "${VOLUME_SRC_MOUNT}" ]; then echo "Missing env VOLUME_SRC_MOUNT in .env file"; exit 1; fi
-if [ -z "${VOLUME_OUTPUTS_NAME}" ]; then echo "Missing env VOLUME_OUTPUTS_NAME in .env file"; exit 1; fi
-if [ -z "${VOLUME_OUTPUTS_DIR}" ]; then echo "Missing env VOLUME_OUTPUTS_DIR in .env file"; exit 1; fi
+if [ -z "${VOLUME_OUTPUTS_NAME_BASE}" ]; then echo "Missing env VOLUME_OUTPUTS_NAME_BASE in .env file"; exit 1; fi
 if [ -z "${VOLUME_OUTPUTS_MOUNT}" ]; then echo "Missing env VOLUME_OUTPUTS_MOUNT in .env file"; exit 1; fi
+if [ -z "${JOBS_DIR}" ]; then echo "Missing env JOBS_DIR in .env file"; exit 1; fi
 if [ -z "${CONTAINER_WORKDIR}" ]; then echo "Missing env CONTAINER_WORKDIR in .env file"; exit 1; fi
 if [ -z "${CONTAINER_CMD}" ]; then echo "Missing env CONTAINER_CMD in .env file"; exit 1; fi
 if [ -z "${OVHAI_CPU_COUNT}" ]; then echo "Missing env OVHAI_CPU_COUNT in .env file"; exit 1; fi
@@ -29,18 +27,29 @@ if [ -z "${PARAMSJSON_FILE}" ]; then echo "Missing env PARAMSJSON_FILE in .env f
 DOCKER_REGISTRY_PREFIX=$(cat "${DIR}/docker-registry-prefix.value")
 if [ -z "${DOCKER_REGISTRY_PREFIX}" ]; then echo "Missing docker registry prefix in in env/docker-registry-prefix.value. Di you publish the image?"; exit 1; fi
 
+echo "Preparing job directory"
+JOB_ID="$(date +"%Y-%m-%d-%H%M")-$(xxd -l2 -ps /dev/urandom)"
+JOB_DIR="${JOBS_DIR}/${JOB_ID}"
+VOLUME_OUTPUTS_NAME="${VOLUME_OUTPUTS_NAME_BASE}${JOB_ID}"
+VOLUME_OUTPUTS_DIR="${JOB_DIR}/outputs"
+mkdir -p "${VOLUME_OUTPUTS_DIR}"
+touch "${VOLUME_OUTPUTS_DIR}/.keep"
+echo "Done"
+echo ""
+
 echo "Uploading volumes"
 ./ovhai data upload "${OVHAI_REGION}" "${VOLUME_DATA_NAME}" "${VOLUME_DATA_DIR}" --remove-prefix "${VOLUME_DATA_DIR}/" &
 ./ovhai data upload "${OVHAI_REGION}" "${VOLUME_SRC_NAME}" "${VOLUME_SRC_DIR}" --remove-prefix "${VOLUME_SRC_DIR}/" &
 ./ovhai data upload "${OVHAI_REGION}" "${VOLUME_OUTPUTS_NAME}" "${VOLUME_OUTPUTS_DIR}" --remove-prefix "${VOLUME_OUTPUTS_DIR}/" &
 wait
 echo "Done uploading volumes"
-
+echo ""
 
 echo "Starting training job on OVHAI"
-# Run the job
 IMAGE_PATH="${DOCKER_REGISTRY_PREFIX}/${IMAGE_NAME}"
 PARAMSJSON=$(cat "${PARAMSJSON_FILE}" | jq -c)
+JOB_STATUS_JSON_FILE="${JOB_DIR}/status.json"
+JOB_STATUS_ID_FILE="${JOB_DIR}/id"
 
 ./ovhai job run "${IMAGE_PATH}" \
     --cpu "${OVHAI_CPU_COUNT}" --gpu "${OVHAI_GPU_COUNT}" \
