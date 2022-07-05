0abcafce6c342710b8e53fcb9fd371ea4c3e11d1
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-07-05 12:26:03 +0200

    refactor: simplify path in env/build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -7,7 +7,7 @@ source "${PROJECT_DIR}/.env"
 if [ -z "${IMAGE_NAME}" ]; then echo "Missing env IMAGE_NAME in .env"; exit 1; fi
 if [ -z "${IMAGE_DIR}" ]; then echo "Missing env IMAGE_DIR in .env"; exit 1; fi
 
-DOCKER_REGISTRY_PREFIX_VALUEFILE="${DIR}/../env/docker-registry-prefix.value"
+DOCKER_REGISTRY_PREFIX_VALUEFILE="${DIR}/docker-registry-prefix.value"
 DOCKER_REGISTRY_PREFIX=$(cat "${DOCKER_REGISTRY_PREFIX_VALUEFILE}")
 if [ -z "${DOCKER_REGISTRY_PREFIX}" ]; then echo "Could not load DOCKER_REGISTRY_PREFIX from valuefile ${DOCKER_REGISTRY_PREFIX_VALUEFILE}"; exit 1; fi
 
