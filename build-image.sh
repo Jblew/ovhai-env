52a6fbba5aae9f036eef99f95653f1bb73b7fad1
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-07-05 12:27:32 +0200

    refactor: image id file path in env settings

--- build-image.sh
+++ build-image.sh
@@ -6,6 +6,7 @@ PROJECT_DIR="${DIR}/.."
 source "${PROJECT_DIR}/.env"
 if [ -z "${IMAGE_NAME}" ]; then echo "Missing env IMAGE_NAME in .env"; exit 1; fi
 if [ -z "${IMAGE_DIR}" ]; then echo "Missing env IMAGE_DIR in .env"; exit 1; fi
+if [ -z "${IMAGE_ID_OUT_FILE}" ]; then echo "Missing env IMAGE_ID_OUT_FILE in .env"; exit 1; fi
 
 DOCKER_REGISTRY_PREFIX_VALUEFILE="${DIR}/docker-registry-prefix.value"
 DOCKER_REGISTRY_PREFIX=$(cat "${DOCKER_REGISTRY_PREFIX_VALUEFILE}")
@@ -17,5 +18,5 @@ docker tag ${IMAGE_NAME} "${DOCKER_REGISTRY_PREFIX}/${IMAGE_NAME}"
 
 echo "Pusing ${DOCKER_REGISTRY_PREFIX}/${IMAGE_NAME} to registry"
 docker push "${DOCKER_REGISTRY_PREFIX}/${IMAGE_NAME}"
-echo -n "$(docker images -q ${IMAGE_NAME} | awk 'NR==1')" > "${PROJECT_DIR}/.image.id"
+echo -n "$(docker images -q ${IMAGE_NAME} | awk 'NR==1')" > "${IMAGE_ID_OUT_FILE}"
 echo "Done"
